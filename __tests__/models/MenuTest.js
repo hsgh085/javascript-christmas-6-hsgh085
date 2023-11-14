6a6c5e07d83bb7a61e8a31604902079391f83d45
hsgh085 <hsyu0917@gmail.com>
2023-11-14 21:16:23 +0900

    test: 카테고리별 주문 메뉴 개수 계산

--- __tests__/models/MenuTest.js
+++ __tests__/models/MenuTest.js
@@ -20,4 +20,14 @@ describe('Menu 모델 테스트', () => {
     //then
     expect(menu.getTotalCount()).toBe(9);
   });
+
+  test('주문 메뉴의 카테고리별 개수를 반환한다', () => {
+    const menuArray = ['양송이수프', '타파스', '제로콜라'];
+    const countArray = [2, 3, 4];
+    const result = { APPETIZER: 5, MAIN: 0, DESSERT: 0, BEVERAGE: 4 };
+    //when
+    const menu = new Menu(menuArray, countArray);
+    //then
+    expect(menu.getCountMenuByCategory()).toEqual(result);
+  });
 });
