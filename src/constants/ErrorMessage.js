3178edef0ae9dbfbe73d58a7b8cd92c3dd3a1b0b
hsgh085 <hsyu0917@gmail.com>
2023-11-14 15:53:13 +0900

    refactor: 에러 메시지 접미사를 상수로 분리

--- src/constants/ErrorMessage.js
+++ src/constants/ErrorMessage.js
@@ -1,8 +1,9 @@
 const ERROR_MESSAGE = Object.freeze({
   PREFIX: '[ERROR] ',
-  DATE: '유효하지 않은 날짜입니다. 다시 입력해 주세요.',
-  MENU: '유효하지 않은 주문입니다. 다시 입력해 주세요.',
-  TOTAL_COUNT: '메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다. 다시 입력해 주세요.',
+  SUFFIX: ' 다시 입력해 주세요.',
+  DATE: '유효하지 않은 날짜입니다.',
+  MENU: '유효하지 않은 주문입니다.',
+  TOTAL_COUNT: '메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다.',
 });
 
 export default ERROR_MESSAGE;
--- src/error/InputError.js
+++ src/error/InputError.js
@@ -2,7 +2,7 @@ import ERROR_MESSAGE from '../constants/ErrorMessage';
 
 class InputError extends Error {
   constructor(message) {
-    super(ERROR_MESSAGE.PREFIX + message);
+    super(ERROR_MESSAGE.PREFIX + message + ERROR_MESSAGE.SUFFIX);
     this.name = 'InputError';
   }
 }
