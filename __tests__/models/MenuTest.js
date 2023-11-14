678d4ea5718b3fd5b5bb020e9b3e7953d8465afc
hsgh085 <hsyu0917@gmail.com>
2023-11-14 15:48:39 +0900

    test: 주문 메뉴 총 개수 계산

--- __tests__/models/MenuTest.js
+++ __tests__/models/MenuTest.js
@@ -3,12 +3,21 @@ import Menu from '../../src/models/Menu';
 describe('Menu 모델 테스트', () => {
   test('주문 메뉴를 입력받으면 메뉴 객체가 생성된다', () => {
     //given
-    const menuArray = ['피자', '햄버거', '치킨'];
+    const menuArray = ['양송이수프', '타파스', '제로콜라'];
     const countArray = [2, 3, 4];
-    const result = { 피자: 2, 햄버거: 3, 치킨: 4 };
+    const result = { 양송이수프: 2, 타파스: 3, 제로콜라: 4 };
     //when
     const menu = new Menu(menuArray, countArray);
     //then
     expect(menu.getMenu()).toEqual(result);
+  });
+
+  test('주문 메뉴의 총 개수를 반환한다', () => {
+    const menuArray = ['양송이수프', '타파스', '제로콜라'];
+    const countArray = [2, 3, 4];
+    //when
+    const menu = new Menu(menuArray, countArray);
+    //then
+    expect(menu.getTotalCount()).toBe(9);
   });
 });
