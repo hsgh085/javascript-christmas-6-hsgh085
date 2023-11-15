3b00cd244529ea8a4943b4781b8d22378c419a8d
hsgh085 <hsyu0917@gmail.com>
2023-11-15 15:27:56 +0900

    refactor: 중복 코드 분리

--- __tests__/models/MenuTest.js
+++ __tests__/models/MenuTest.js
@@ -1,41 +1,24 @@
 import Menu from '../../src/models/Menu';
 
 describe('Menu 모델 테스트', () => {
+  const menuArray = ['양송이수프', '타파스', '제로콜라'];
+  const countArray = [2, 3, 4];
+  const menu = new Menu(menuArray, countArray);
+
   test('주문 메뉴를 입력받으면 메뉴 객체가 생성된다', () => {
-    //given
-    const menuArray = ['양송이수프', '타파스', '제로콜라'];
-    const countArray = [2, 3, 4];
     const result = { 양송이수프: 2, 타파스: 3, 제로콜라: 4 };
-    //when
-    const menu = new Menu(menuArray, countArray);
-    //then
     expect(menu.getMenu()).toEqual(result);
   });
 
   test('주문 메뉴의 총 개수를 반환한다', () => {
-    const menuArray = ['양송이수프', '타파스', '제로콜라'];
-    const countArray = [2, 3, 4];
-    //when
-    const menu = new Menu(menuArray, countArray);
-    //then
     expect(menu.getTotalCount()).toBe(9);
   });
 
   test('주문 메뉴의 총 가격을 반환한다', () => {
-    const menuArray = ['양송이수프', '타파스', '제로콜라'];
-    const countArray = [2, 3, 4];
-    //when
-    const menu = new Menu(menuArray, countArray);
-    //then
     expect(menu.getTotalPrice()).toBe(40500);
   });
 
   test('주문 메뉴의 특정 카테고리 개수를 반환한다', () => {
-    const menuArray = ['양송이수프', '타파스', '제로콜라'];
-    const countArray = [2, 3, 4];
-    //when
-    const menu = new Menu(menuArray, countArray);
-    //then
     expect(menu.getCountCategory('BEVERAGE')).toBe(4);
   });
 });
