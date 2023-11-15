67aa031e668b849d12c87698409a81346bf40316
hsgh085 <hsyu0917@gmail.com>
2023-11-15 16:02:55 +0900

    test: Events 모델 테스트

--- __tests__/models/EventsTest.js
+++ __tests__/models/EventsTest.js
@@ -9,4 +9,78 @@ describe('Events 모델 테스트', () => {
     //then
     expect(events.isApplyEvent()).toBeTruthy();
   });
+
+  test('총 금액 12만원 이상이면 샴페인 1개(25,000원) 증정한다', () => {
+    //given
+    const totalPrice = 120000;
+    //when
+    const events = new Events(totalPrice);
+    events.applyGiftEvent();
+    //then
+    expect(events.getEventsDiscount().gift).toBe(-25000);
+  });
+
+  test('평일 이벤트면 디저트 1개당 2,023원 할인한다', () => {
+    //given
+    const totalPrice = 10000;
+    const day = 'weekday';
+    const dessert_count = 2;
+    //when
+    const events = new Events(totalPrice);
+    events.applyWeekOrWeekendEvent(day, dessert_count);
+    //then
+    expect(events.getEventsDiscount().weekday).toBe(-4046);
+  });
+
+  test('주말 이벤트면 메인 1개당 2,023원 할인한다', () => {
+    //given
+    const totalPrice = 10000;
+    const day = 'weekend';
+    const main_count = 2;
+    //when
+    const events = new Events(totalPrice);
+    events.applyWeekOrWeekendEvent(day, main_count);
+    //then
+    expect(events.getEventsDiscount().weekend).toBe(-4046);
+  });
+
+  test('특별 할인 이벤트면 천원 할인한다', () => {
+    //given
+    const totalPrice = 10000;
+    //when
+    const events = new Events(totalPrice);
+    events.applySpecialEvent();
+    //then
+    expect(events.getEventsDiscount().special).toBe(-1000);
+  });
+
+  test('크리스마스 이벤트면 1000원 할인에서 하루 지날수록 100원씩 추가 할인한다.', () => {
+    //given
+    const totalPrice = 10000;
+    const date = 25;
+    //when
+    const events = new Events(totalPrice);
+    events.applyChristmasEvent(date);
+    //then
+    expect(events.getEventsDiscount().christmas).toBe(-3400);
+  });
+
+  test('증정 할인이 0원이면 증정 메뉴 개수는 0이다.', () => {
+    //given
+    const totalPrice = 10000;
+    //when
+    const events = new Events(totalPrice);
+    //then
+    expect(events.getGiftCount()).toBe(-0);
+  });
+
+  test('총 혜택 금액이 0원이면 할인 전 총주문 금액과 할인 후 예상 결제 금액은 같다', () => {
+    //given
+    const totalPrice = 10000;
+    //when
+    const events = new Events(totalPrice);
+    //then
+    expect(events.getTotalDiscount()).toBe(0);
+    expect(events.getAfterDiscountPrice()).toBe(totalPrice);
+  });
 });
