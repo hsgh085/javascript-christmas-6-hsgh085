5c9fdb10f457be3fe080b73caee8108470f957ac
hsgh085 <hsyu0917@gmail.com>
2023-11-15 16:17:11 +0900

    refactor: beforeEach를 사용해 중복 코드 제거

--- __tests__/models/EventsTest.js
+++ __tests__/models/EventsTest.js
@@ -1,15 +1,6 @@
 import Events from '../../src/models/Events';
 
 describe('Events 모델 테스트', () => {
-  test('총 금액 만원 이상이면 true를 반환한다.', () => {
-    //given
-    const totalPrice = 10000;
-    //when
-    const events = new Events(totalPrice);
-    //then
-    expect(events.isApplyEvent()).toBeTruthy();
-  });
-
   test('총 금액 12만원 이상이면 샴페인 1개(25,000원) 증정한다', () => {
     //given
     const totalPrice = 120000;
@@ -20,13 +11,22 @@ describe('Events 모델 테스트', () => {
     expect(events.getEventsDiscount().gift).toBe(-25000);
   });
 
+  const totalPrice = 10000;
+  let events;
+
+  beforeEach(() => {
+    events = new Events(totalPrice);
+  });
+
+  test('총 금액 만원 이상이면 true를 반환한다.', () => {
+    expect(events.isApplyEvent()).toBeTruthy();
+  });
+
   test('평일 이벤트면 디저트 1개당 2,023원 할인한다', () => {
     //given
-    const totalPrice = 10000;
     const day = 'weekday';
     const dessert_count = 2;
     //when
-    const events = new Events(totalPrice);
     events.applyWeekOrWeekendEvent(day, dessert_count);
     //then
     expect(events.getEventsDiscount().weekday).toBe(-4046);
@@ -34,52 +34,33 @@ describe('Events 모델 테스트', () => {
 
   test('주말 이벤트면 메인 1개당 2,023원 할인한다', () => {
     //given
-    const totalPrice = 10000;
     const day = 'weekend';
     const main_count = 2;
     //when
-    const events = new Events(totalPrice);
     events.applyWeekOrWeekendEvent(day, main_count);
     //then
     expect(events.getEventsDiscount().weekend).toBe(-4046);
   });
 
   test('특별 할인 이벤트면 천원 할인한다', () => {
-    //given
-    const totalPrice = 10000;
-    //when
-    const events = new Events(totalPrice);
     events.applySpecialEvent();
-    //then
     expect(events.getEventsDiscount().special).toBe(-1000);
   });
 
   test('크리스마스 이벤트면 1000원 할인에서 하루 지날수록 100원씩 추가 할인한다.', () => {
     //given
-    const totalPrice = 10000;
     const date = 25;
     //when
-    const events = new Events(totalPrice);
     events.applyChristmasEvent(date);
     //then
     expect(events.getEventsDiscount().christmas).toBe(-3400);
   });
 
   test('증정 할인이 0원이면 증정 메뉴 개수는 0이다.', () => {
-    //given
-    const totalPrice = 10000;
-    //when
-    const events = new Events(totalPrice);
-    //then
     expect(events.getGiftCount()).toBe(-0);
   });
 
   test('총 혜택 금액이 0원이면 할인 전 총주문 금액과 할인 후 예상 결제 금액은 같다', () => {
-    //given
-    const totalPrice = 10000;
-    //when
-    const events = new Events(totalPrice);
-    //then
     expect(events.getTotalDiscount()).toBe(0);
     expect(events.getAfterDiscountPrice()).toBe(totalPrice);
   });
